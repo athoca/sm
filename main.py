f0fe1c375939146c35315a6882bb761a08b41631
chaunguyen <athoca.nguyen@gmail.com>
2021-09-26 16:46:53 +0200

    correct again

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from smartdrone.utils import is_on_Xavier
-if is_on_Xavier():
+import os
+name = os.uname()[1]
+if name == "bootai":
     import sys
     sys.path.append('/home/bootai/.local/lib/python3.6/site-packages')
-
 
 from smartdrone.utils import sd_logger
 from smartdrone.vehicles import connect_to_PLdrone
